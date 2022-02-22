c1c289143e8ce61bf28c639d2f42ccd9d858a12b
Andriis09 <tandris09@gmail.com>
2022-02-22 13:18:50 +0100

    otodik

---
+++ script.js
@@ -0,0 +1,17 @@
+function Nyom(){
+let pont=0;
+if(document.getElementById("Kocs KSE").checked==true){
+    pont++;
+}
+pont++;
+if(document.getElementById("Kocs KSE").checked==true){pont++
+} 
+
+if(document.getElementById("fovarosa").value=="Igaz"){pont++
+} 
+if(document.getElementById("Kocs KSE").checked==true){pont++
+} 
+
+
+
+}
