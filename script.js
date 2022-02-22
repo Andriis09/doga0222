29049a6a975f5f4191fee00f0fd3e36d3d54981d
Andriis09 <tandris09@gmail.com>
2022-02-22 13:23:48 +0100

    hetedik

--- script.js
+++ script.js
@@ -9,9 +9,13 @@ if(document.getElementById("Kocs KSE").checked==true){pont++
 
 if(document.getElementById("fovarosa").value=="Igaz"){pont++
 } 
-if(document.getElementById("Kocs KSE").checked==true){pont++
+
+if(document.getElementById("Lisszabon").checked==true){pont++
 } 
 
+if(document.getElementById("1").checked==true){pont++
+} 
 
-
+if(document.getElementById("2").checked==true){pont++
+} 
 }
